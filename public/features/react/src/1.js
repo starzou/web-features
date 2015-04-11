cb0b2ea8c1b60864b93dfe0c10e82b25a66efad6
StarZou <zoutongyuan@163.com>
2015-04-11 11:25:48 +0800

    react 研究

--- public/features/react/src/1.js
+++ public/features/react/src/1.js
@@ -5,4 +5,25 @@
  * @author StarZou
  **/
 
-React.render(<h1>hello world!</h1>, document.getElementById('example'));
+var body = document.body,
+    headerDiv = document.getElementById('header'),
+    contentDiv = document.getElementById('content');
+
+React.render(<h1>hello world!</h1>, headerDiv);
+
+var HobbyBox = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <label>爱好：</label>
+                <select>
+                    <option>value 1</option>
+                    <option>value 2</option>
+                    <option>value 3</option>
+                </select>
+            </div>
+        );
+    }
+});
+
+React.render(<HobbyBox />, contentDiv);
