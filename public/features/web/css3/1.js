dd84a3b25d6ff82efa6e00ae85d7b5bea22b0c45
StarZou <zoutongyuan@163.com>
2015-04-18 15:35:49 +0800

    css3 研究

--- public/features/web/css3/1.js
+++ public/features/web/css3/1.js
@@ -6,9 +6,25 @@
  **/
 
 var title = document.querySelector('.title');
-
 var btnTransition = document.getElementById('btnTransition');
+var btnAnimation = document.getElementById('btnAnimation');
 
+/**
+ * 过渡效果
+ */
 btnTransition.addEventListener('click', function () {
     title.classList.toggle('new-title');
+});
+
+
+/**
+ * 动画效果
+ */
+var animationClassName = 'animation-title';
+btnAnimation.addEventListener('click', function () {
+    title.classList.add(animationClassName);
+});
+
+title.addEventListener('webkitAnimationEnd', function () {
+    this.classList.remove(animationClassName);
 });
