f8980426b39d6e7b6040cae82787d3b496e50a95
StarZou <zoutongyuan@163.com>
2015-04-23 11:53:05 +0800

    es6 研究

--- public/features/es6/1.js
+++ public/features/es6/1.js
@@ -4,3 +4,17 @@
  * @time 2015-04-09 11:44
  * @author StarZou
  **/
+
+"use strict";
+
+(function testLet() {
+
+    {
+        var name = 'StarZou';
+        let age = 24;
+
+        console.log(age);
+    }
+
+    console.log('%s age is %d', name, typeof age != 'undefined' ? age : 0);
+})();
