166ab2a95ea8141723795395df73fb7a7711de4c
StarZou <zoutongyuan@163.com>
2015-04-28 09:48:21 +0800

    md 研究

---
+++ public/features/angular-material/1.js
@@ -0,0 +1,15 @@
+/**
+ * @class 1
+ * @description
+ * @time 2015-04-27 18:17
+ * @author StarZou
+ **/
+
+
+var app = angular.module('App', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
+    $scope.toggleSidenav = function (menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+}]);
