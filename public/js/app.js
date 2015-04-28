2ba269e42fd26a41587f8f2e989f14d89153a226
StarZou <zoutongyuan@163.com>
2015-04-28 14:29:57 +0800

    ng 研究

--- public/js/app.js
+++ public/js/app.js
@@ -13,4 +13,37 @@
         $scope.title = 'know more web-features';
     }]);
 
+    /**
+     * 测试ng 指令执行过程
+     */
+    app.directive('ngFlow', [function () {
+        return {
+            restrict   : 'EA',
+            scope      : true,
+            templateUrl: 'js/ng-flow.tpl.html',
+            controller : ['$scope', '$attrs', function ($scope, $attrs) {
+                console.log('%s controller ...', $attrs.name);
+            }],
+            compile    : function ($element, $attrs) {
+                console.log('%s compile ...', $attrs.name);
+
+                return {
+                    pre: function ngFlowPreLink($scope, $element, $attrs, ctrls) {
+                        console.log('%s pre ...', $attrs.name);
+                    },
+
+                    post: function ngFlowPreLink($scope, $element, $attrs, ctrls) {
+                        console.log('%s post ...', $attrs.name);
+
+                        angular.forEach(['name'], function (key) {
+                            if (angular.isDefined($attrs[key])) {
+                                $scope[key] = $attrs[key];
+                            }
+                        });
+                    }
+                };
+            }
+        }
+    }]);
+
 })(window, document);
