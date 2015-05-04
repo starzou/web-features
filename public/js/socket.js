f17c3607b72a948575689c86bb588070db21744e
StarZou <zoutongyuan@163.com>
2015-05-04 18:37:19 +0800

    socket.io 研究

---
+++ public/js/socket.js
@@ -0,0 +1,12 @@
+/**
+ * @class socket
+ * @description socket 客户端
+ * @time 2015-05-04 18:35
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+
+    var socket = io();
+
+})(window, document);
