5981a9167b65039636b798bcf67e30bd46857c8f
StarZou <zoutongyuan@163.com>
2015-05-04 10:56:47 +0800

    react 研究

--- public/features/react/src/1.js
+++ public/features/react/src/1.js
@@ -11,19 +11,75 @@ var body = document.body,
 
 React.render(<h1>Hello world!</h1>, headerDiv);
 
-var HobbyBox = React.createClass({
+//var HobbyBox = React.createClass({
+//    render: function () {
+//        return (
+//            <div>
+//                <label>爱好：</label>
+//                <select>
+//                    <option>value 1</option>
+//                    <option>value 2</option>
+//                    <option>value 3</option>
+//                </select>
+//            </div>
+//        );
+//    }
+//});
+
+//React.render(<HobbyBox />, contentDiv);
+
+var CommentBox = React.createClass({
     render: function () {
         return (
-            <div>
-                <label>爱好：</label>
-                <select>
-                    <option>value 1</option>
-                    <option>value 2</option>
-                    <option>value 3</option>
-                </select>
+            <div className="comment-box">
+                <h1>Hello, world! I am a CommentBox.</h1>
+                <CommentList />
+                <CommentForm />
             </div>
         );
     }
 });
 
-React.render(<HobbyBox />, contentDiv);
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="comment-list">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="comment-form">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="comment-author">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+var data = [
+    {author: "Pete Hunt", text: "This is one comment"},
+    {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+React.render(
+    <CommentBox data={data}/>,
+    contentDiv
+);
