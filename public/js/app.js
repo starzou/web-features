0ecf926ee1fb56643fe6102ce1f8cfaa80c68810
StarZou <zoutongyuan@163.com>
2015-05-05 12:50:18 +0800

    socket.io 研究

--- public/js/app.js
+++ public/js/app.js
@@ -13,4 +13,38 @@
         $scope.title = 'know more web-features';
     }]);
 
+    app.directive('chatForm', [function () {
+        return {
+            restrict   : 'E',
+            scope      : true,
+            templateUrl: 'js/chat-form.tpl.html',
+            link       : function ($scope, $element, $attrs) {
+                // socket 对象
+                var socket = $scope.socket;
+
+                // 发送消息
+                $scope.sendMessage = function (message) {
+                    socket.emit('message:send', message);
+                    message.content = null;
+                };
+
+                // 更新消息
+                socket.on('message:update', function (message) {
+                    $scope.$apply(function () {
+                        console.log('message : ', message);
+                        $scope.messages.push(message);
+                    });
+                });
+
+                // 同步消息
+                socket.on('message:list', function (messages) {
+                    $scope.$apply(function () {
+                        console.log('messages : ', messages);
+                        $scope.messages = messages;
+                    });
+                });
+            }
+        };
+    }]);
+
 })(window, document);
