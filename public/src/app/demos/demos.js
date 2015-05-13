092ebeca62266231f2f35d96b85f58599f57e41a
StarZou <zoutongyuan@163.com>
2015-05-13 22:49:52 +0800

    web-features 开发

---
+++ public/src/app/demos/demos.js
@@ -0,0 +1,20 @@
+/**
+ * @class demos
+ * @description demos 模块
+ * @time 2015-05-13 22:45
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
+
+    var demosModule = angular.module('demos', []);
+
+    demosModule.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('demos', {
+                url        : '/demos',
+                templateUrl: 'app/demos/demos.tpl.html'
+            });
+    }]);
+
+})(window, document);
