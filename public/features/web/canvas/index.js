228349480c62fe7ba61c285363b1bc33be15dc1d
StarZou <zoutongyuan@163.com>
2015-05-28 09:05:29 +0800

    canvas 研究

--- public/features/web/canvas/index.js
+++ public/features/web/canvas/index.js
@@ -4,7 +4,35 @@
  * @time 2015-05-28 08:43
  * @author StarZou
  **/
+
+var $ = window.$ = function (selector) {
+    return document.querySelector(selector);
+};
+
+$.on = function (element, type, listener) {
+    element.addEventListener(type, listener, false);
+
+    return function () {
+        element.removeEndEventListener(type, listener);
+    };
+};
+
 (function (window, document) {
     'use strict';
+
+    var displayImage = $('#display');
+
+    var drawingCanvas = $('#drawing');
+
+    var context = drawingCanvas.getContext('2d');
+
+    var saveAsImageButton = $('#saveAsImageButton');
+
+    $.on(saveAsImageButton, 'click', function () {
+        var imgUrl = drawingCanvas.toDataURL('image/png');
+        displayImage.src = imgUrl;
+    });
+
+    console.log(context);
 
 })(window, document);
