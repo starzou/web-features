0d6a9548a31a343e9a0fc55b96c7a1403c7cef9b
StarZou <zoutongyuan@163.com>
2015-05-29 21:03:05 +0800

    研究

--- mocker/routes/images.js
+++ mocker/routes/images.js
@@ -16,14 +16,17 @@ router.get('/images', function (request, response) {
     var data = {
         list: [
             {
+                id   : 1,
                 title: '雪糕校花网络爆红 纯美女神青春风暴又临',
                 url  : 'http://s1.dwstatic.com/group1/M00/A8/6F/f759f70958f620132e538002ca11c8f0.jpg'
             },
             {
+                id   : 2,
                 title: '台湾游戏嫩模大尺度私照引发网友热议',
                 url  : 'http://s1.dwstatic.com/group1/M00/62/EB/d53eac544adf4db060b8aa7b67f3d579.jpg'
             },
             {
+                id   : 3,
                 title: 'DOTA2痛苦女王COSER郭婷瑜海量私房照曝光',
                 url  : 'http://s1.dwstatic.com/group1/M00/E2/1F/66eb5ab3d934fc671db21f9a0da24cb3.jpg'
             }
