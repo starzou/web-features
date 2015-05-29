0036e25d73b6ddd0983ef206601a6c2e6c6d7865
StarZou <zoutongyuan@163.com>
2015-05-29 14:30:13 +0800

    研究

--- mocker/routes/auth.js
+++ mocker/routes/auth.js
@@ -14,16 +14,28 @@ var express = require('express'),
  */
 router.post('/auth', function (request, response) {
     var user = request.body;
-    console.log(user);
 
-    response.send(new Result());
+    var result = new Result();
+
+    if (user) {
+        if (user.userName === 'star' && user.password === '123456') {
+            result.success = true;
+            result.message = '验证成功';
+        } else {
+            result.message = '用户名或密码错误';
+        }
+    } else {
+        result.message = '请输入用户名密码';
+    }
+
+    response.send(result);
 });
 
 module.exports = router;
 
 
 function Result(success, data, message, code) {
-    this.success = success || true;
+    this.success = success || false;
     this.data = data || null;
     this.message = message || null;
     this.code = code || null;
