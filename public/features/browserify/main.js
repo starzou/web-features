9705cd88216531cbb95abd9295d7bbc23ad844cc
StarZou <zoutongyuan@163.com>
2015-06-11 09:51:31 +0800

    browserify 研究

---
+++ public/features/browserify/main.js
@@ -0,0 +1,12 @@
+/**
+ * @class main
+ * @description main module
+ * @time 2015-06-11 09:40
+ * @author StarZou
+ **/
+
+var unique = require('uniq');
+
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+console.log(unique(data));
