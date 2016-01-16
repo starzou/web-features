9c9ba1ea2a969ce2c5ae55403a3f30d30896dd5c
StarZou <starzou@126.com>
2016-01-16 11:57:32 +0800

    ECMAScript

--- public/features/ECMAScript-2015/test2.js
+++ public/features/ECMAScript-2015/test2.js
@@ -14,10 +14,17 @@ let Test2 = {
     /**
      * 常量(const)规则:
      * 1. 声明常量, 必须设置初始值.
-     * 2. 常量不能再赋值
+     * 2. 常量不能再赋值、改变
      * 3. 常量不能再声明
+     * 4. 常量拥有块作用域
+     * 5. 常量不能和它所在作用域内的其他变量或函数拥有相同的名称
      */
     const MY_NAME = 'StarZou';
+
+    {
+      const MY_NAME = 'Block Scope-StarZou';
+      console.log(MY_NAME);
+    }
 
     console.log(MY_NAME);
   }
