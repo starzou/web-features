a0d61718da7deb6f64abfd98e8ee1fc89a32b894
StarZou <starzou@126.com>
2016-01-19 22:09:56 +0800

    ECMAScript

--- public/features/test/gdp.js
+++ public/features/test/gdp.js
@@ -36,12 +36,12 @@ for (var index = 0; index < length; index++) {
 
   for (var j = 0; j < cells.length; j++) {
     cell = cells[j];
-    textArray.push(cell.outerText);
+    textArray.push(cell.innerText);
   }
 
   if (textArray[4].indexOf('（') < 0) {
     //console.log(textArray);
-    tableData.push(new Type(textArray[0], textArray[1], textArray[2], textArray[3], textArray[4]));
+    tableData.push(new Type(textArray[0].split('\n')[0], textArray[1], textArray[2], textArray[3], textArray[4]));
   }
 }
 
@@ -49,6 +49,6 @@ var me = {
   name: '2015年12月份及全年主要统计数据',
   data: tableData
 };
-console.log(me);
+//console.log(me);
 
 console.log(JSON.stringify(me));
