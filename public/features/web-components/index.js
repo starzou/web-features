e47b27dde21be8587ac9de8a47319fc61df62af8
StarZou <starzou@126.com>
2016-02-25 18:44:37 +0800

    WebComponents

--- public/features/web-components/index.js
+++ public/features/web-components/index.js
@@ -5,27 +5,50 @@
  * @author StarZou
  **/
 
-/**
- *  Custom Elements 使用
- */
-var AnchorPrototype = Object.create(HTMLElement.prototype);
+// Create a new object based of the HTMLElement prototype
+var XColumnsProto = Object.create(HTMLElement.prototype);
+var XColumnProto = Object.create(HTMLElement.prototype);
 
+XColumnsProto.createdCallback = function () {
+  // Create a Shadow Root
 
-AnchorPrototype.createdCallback = function () {
-    var style = this.style;
-    this.textContent = this.attributes.name.textContent + '-' + Date.now();
-    style.display = 'block';
-    style.color = 'red';
+  var size = +this.getAttribute('size') || 0;
+
+  var html = '', shadow;
+
+  if (size > 0) {
+
+    for (var index = 0; index < size; index++) {
+      html += '<x-column></x-column>';
+    }
+
+    shadow = this.createShadowRoot();
+    shadow.innerHTML = html;
+  }
+
+  console.log(size);
 };
 
 
-document.registerElement('data-anchor', {prototype: AnchorPrototype});
+XColumnProto.createdCallback = function () {
+  // Create a Shadow Root
+  var shadow = this.createShadowRoot();
 
+  var div = document.createElement('div');
 
-/**
- * Shadow DOM
- */
-var tip = document.createElement('div');
-var shadowRoot = tip.createShadowRoot();
-shadowRoot.innerHTML = '<label>Name:</label><input type="text" value="StarZou"/>';
-document.body.appendChild(tip);
+  div.style.display = 'inline-block';
+  div.style.height = '200px';
+  div.style.width = '200px';
+  div.style.border = '1px solid #2196F3';
+
+  shadow.appendChild(div);
+};
+
+// Register the new element.
+document.registerElement('x-columns', {
+  prototype: XColumnsProto
+});
+
+document.registerElement('x-column', {
+  prototype: XColumnProto
+});
